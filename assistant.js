4dde3b3b40388b525fa9d252f6c51cbdee37431e
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-13 13:46:01 -0700

    Add methods

--- assistant.js
+++ assistant.js
@@ -9,7 +9,26 @@ function PersonalAssistant(name) {
 
 
 // Define at least three methods:
+PersonalAssistant.prototype.addTask = function(task) {
+    this.tasks.push(task);
+    console.log(`New task added: ${task}`);
+};
 
+PersonalAssistant.prototype.completeTask = function() {
+    if (this.tasks.length > 0) {
+        const completedTask = this.tasks.shift();
+        console.log(`Completing task: ${completedTask}`);
+        this.updateMood("accomplished");
+        console.log(`Now you have ${this.tasks.length} tasks left.`);
+    } else {
+        console.log("No tasks left to complete.");
+    }
+};
 
+PersonalAssistant.prototype.reportMood = function() {
+    console.log(`Mood: ${this.mood}`);
+};
 
-// Use console.log() to simulate a day in your life where you:
+PersonalAssistant.prototype.updateMood = function(newMood) {
+    this.mood = newMood;
+};
