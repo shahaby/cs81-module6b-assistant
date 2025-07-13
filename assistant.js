c77b7bd42fd790aa5683b3f876a70c44c10a189e
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-13 13:31:33 -0700

    Add constructor function

--- assistant.js
+++ assistant.js
@@ -1,2 +1,15 @@
 // https://github.com/shahaby/cs81-module6b-assistant
 
+// Define a constructor function PersonalAssistant(name)
+function PersonalAssistant(name) {
+    this.name = name;
+    this.tasks = [];
+    this.mood = "neutral";
+}
+
+
+// Define at least three methods:
+
+
+
+// Use console.log() to simulate a day in your life where you:
